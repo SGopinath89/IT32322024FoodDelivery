59fd0f66af3baacac6096867e663379cf9746e8a
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-05-15 18:17:27 +0530

    Add Routes

---
+++ Frontend/src/Routers/CustomerRouter.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from '../component/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from '../component/Home/Home';
+import RestaurantDetails from '../component/Restaurant/RestaurantDetails';
+import Cart from '../component/Cart/Cart';
+import Profile from '../component/Profile/Profile';
+import Auth from '../component/Auth/Auth';
+
+
+const CustomerRouter = () => {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+
+                <Route path='/' element={<Home />} />
+                <Route path='/account/:register' element={<Home />} />
+                <Route path='/restaurant/:city/:title/:id' element={<RestaurantDetails />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/profile/*' element={<Profile />} />
+
+              
+                
+                
+
+            </Routes>
+            <Auth/>
+        </div>
+    )
+}
+
+export default CustomerRouter
