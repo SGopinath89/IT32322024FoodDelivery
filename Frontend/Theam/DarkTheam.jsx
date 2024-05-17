5b634a24741470ba2d172490ae402782f8d1bda1
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-05-17 23:28:19 +0530

    Create custom theam

---
+++ Frontend/Theam/DarkTheam.jsx
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheam = createTheme({
+
+    palette: {
+
+        mode: "dark",
+        primary: {
+            main: "#C61C0E"
+        },
+        secondary: {
+            main: "#5A20CB"
+        },
+        black: {
+            main: "#242B2E"
+        },
+        background: {
+            main: "#000000",
+            default: "#0D0D0D",
+            paper: "#0D0D0D"
+        },
+        textColor: {
+            main: "#111111"
+        }
+
+    }
+});
