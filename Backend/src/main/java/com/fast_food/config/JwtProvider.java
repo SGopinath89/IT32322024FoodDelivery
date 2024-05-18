82c9c7ea08ed22e882c4fc04d8d53b50b993ab82
nuflait <nufla2017@gmail.com>
2024-05-18 15:09:45 +0530

    JwtProvider class

---
+++ Backend/src/main/java/com/fast_food/config/JwtProvider.java
@@ -0,0 +1,54 @@
+package com.fast_food.config;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.*;
+
+@Service
+public class JwtProvider {
+
+    private SecretKey key= Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public String generateToken(Authentication auth){
+
+        Collection<? extends GrantedAuthority> authorities =auth.getAuthorities();
+        String roles = populateAuthorities(authorities);  //jwt token allows only string type that's why convert to string again
+
+        String jwt= Jwts.builder().setIssuedAt(new Date())
+                .setExpiration((new Date(new Date().getTime()+8640000)))
+                .claim("email",auth.getName())
+                .claim("authorities",roles)
+                .signWith(key)
+                .compact();
+
+        return  jwt;
+    }
+
+    public String getEmailFromJwtToken(String jwt){
+        jwt=jwt.substring(7);
+
+        Claims claims=Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        String email=String.valueOf(claims.get("email"));
+
+        return email;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+
+        Set<String> auths=new HashSet<>();
+
+        for (GrantedAuthority authority:authorities){
+            auths.add(authority.getAuthority());
+        }
+        return  String.join(",",auths);
+
+    }
+}
