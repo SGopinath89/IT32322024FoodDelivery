3decd7d06c757556bd96bb13b1cecd0cad7e2c9f
nuflait <nufla2017@gmail.com>
2024-05-18 16:05:00 +0530

    Data Transfer Object for restaurant-related data

---
+++ Backend/src/main/java/com/fast_food/dto/RestaurantDto.java
@@ -0,0 +1,21 @@
+package com.fast_food.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+
+    private String title;
+
+    @Column(length = 1000)
+    private List<String> images;
+
+    private String description;
+
+    private Long id;
+}
