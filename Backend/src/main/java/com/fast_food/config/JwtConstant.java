0e9b0763f21ddbac67f0bda40c93a94767f1cd4d
nuflait <nufla2017@gmail.com>
2024-05-18 14:38:28 +0530

    JwtConstant

---
+++ Backend/src/main/java/com/fast_food/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.fast_food.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="dekdsesed54fg524dnkfdskfdfd5e++560949r3422gfmscskf000gdf356rgt+fgkfkfjdkk";
+    public static final String JWT_HEADER="Authorization";
+}
