e935bdec99280eb92a42414b761ff77ee28d0d9c
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-05-18 08:28:49 +0530

    Back end folder create
    add spring folder

---
+++ Backend/src/main/java/com/fast_food/OnlineFoodOrderingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.fast_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderingSystemApplication.class, args);
+		System.out.println("Running Ok");
+	}
+
+}
---
+++ Backend/src/test/java/com/fast_food/OnlineFoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
