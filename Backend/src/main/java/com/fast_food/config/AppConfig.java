de87a13db71a58de2e546fe68b9f1a90cd23b837
nuflait <nufla2017@gmail.com>
2024-05-18 13:30:31 +0530

    Security configuration for a Fast Food

---
+++ Backend/src/main/java/com/fast_food/config/AppConfig.java
@@ -0,0 +1,72 @@
+package com.fast_food.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize->Authorize
+                        .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER","ADMIN")
+                        .requestMatchers("/api/**").authenticated() //only jwt matched users allow
+                        .anyRequest().permitAll() //user do not want to provide jwt token sign in and sign up and
+                        // visit first time
+                ).addFilterBefore(new JwtTokenValidator(),  BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors-> cors.configurationSource(corsConfigrationSource()));
+
+        return  http.build();
+    }
+
+    private CorsConfigurationSource corsConfigrationSource() {
+
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration cfg=new CorsConfiguration();
+
+                cfg.setAllowedOrigins(List.of(
+                        "http://localhost:5173/"
+                ));
+
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+
+                return cfg;
+            }
+        };
+
+
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+}
