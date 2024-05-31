cb06d8a86d2f5fe2ed8907dc340b1523df0e248a
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-05-31 20:57:34 +0530

    Change Background image banner

--- Frontend/src/component/Navbar/Navbar.jsx
+++ Frontend/src/component/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import { Avatar, Badge, IconButton } from '@mui/material';
+import { Alert, Avatar, Badge, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import React from 'react'
+import React, { useState } from 'react'
 import { pink } from '@mui/material/colors';
 import PersonIcon from '@mui/icons-material/Person';
 import { useNavigate } from 'react-router-dom';
@@ -13,9 +13,9 @@ const Navbar = () => {
     const navigate = useNavigate();
 
     const { auth, cart } = useSelector(store => store);
+    const [authenticate, setAuthenticate] = useState(false);
 
     const handleAvatarClick = () => {
-
         if (auth.user.role === "ROLE_CUSTOMER") {
             navigate("/profile");
         } else {
@@ -23,54 +23,55 @@ const Navbar = () => {
         }
     }
 
+    const handleCart = () => {
+        if (auth.user) {
+            navigate("/cart");
+        } else {
+            setAuthenticate(true);
+        }
+    }
+
     return (
-
-        <div className='px-5 sticky top-0 z-50 py-[0.8rem] bg-black lg:px-20 flex justify-between '>
-
+        <div className='px-5 sticky top-0 z-50 py-[0.8rem] bg-black lg:px-20 flex justify-between'>
             <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
                 <li className='list-none logo text-[26px] underline'>
                     <Link to='/'>FAST FOOD</Link>
                 </li>
             </div>
-
             <div className='flex items-center space-x-2 lg:space-x-10'>
-
                 <div>
                     <IconButton>
                         <SearchIcon sx={{ fontSize: "1.5rem" }} />
                     </IconButton>
                 </div>
-
                 <div >
-
-                    {
-                        auth.user ? (
-                            <IconButton>
-                                <Avatar onClick={handleAvatarClick} sx={{ bgcolor: "white", color: pink.A400, width: 32, height: 32 }}>
-                                    {auth.user?.fullName[0].toUpperCase()}
-                                </Avatar>
-                            </IconButton>) :
-                            (<IconButton onClick={() => navigate("/account/login")}><PersonIcon /></IconButton>)
-                    }
-
+                    {auth.user ? (
+                        <IconButton>
+                            <Avatar onClick={handleAvatarClick} sx={{ bgcolor: "white", color: pink.A400, width: 32, height: 32 }}>
+                                {auth.user?.fullName[0].toUpperCase()}
+                            </Avatar>
+                        </IconButton>
+                    ) : (
+                        <IconButton onClick={() => navigate("/account/login")}>
+                            <PersonIcon />
+                        </IconButton>
+                    )}
                 </div>
-
-                <div >
-                    <IconButton onClick={() => navigate("/cart")}>
+                <div>
+                    <IconButton onClick={handleCart}>
                         <Badge color='primary' badgeContent={cart.cart?.item.length}>
-                            <ShoppingCartIcon sx={{ fontSize: "1.5rem" }}></ShoppingCartIcon>
+                            <ShoppingCartIcon sx={{ fontSize: "1.5rem" }} />
                         </Badge>
                     </IconButton>
-
                 </div>
-
             </div>
-
-
-
+            {authenticate && (
+                <Alert severity="error" sx={{ position: 'fixed', top: 10, right: 10 }}>
+                    Please authenticate to access this feature.
+                </Alert>
+            )}
         </div>
-
     )
 }
 
-export default Navbar
+export default Navbar;
