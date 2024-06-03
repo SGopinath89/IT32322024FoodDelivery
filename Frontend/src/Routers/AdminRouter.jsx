2b3ea088741bdcdba25d1f5139be271cd6cc1cf0
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-06-03 13:53:05 +0530

    Fix admin/restaurant login issue
    http://localhost:5173/admin/restaurants
    after enter this part going to create restaurant page now i fix it if not authenticate redirect to login

--- Frontend/src/Routers/AdminRouter.jsx
+++ Frontend/src/Routers/AdminRouter.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import CreateRestaurantForm from '../AdminComponets/CreateAdminRestuarnt/CreateRestaurantForm'
 import Admin from '../AdminComponets/Admin/Admin'
 import { useSelector } from 'react-redux'
 
+
+
 const AdminRouter = () => {
 
-  const { restaurant } = useSelector((store) => store);
+  const { restaurant, auth } = useSelector((store) => store);
+  const navigate = useNavigate();
 
   return (
     <div>
       <Routes>
         <Route>
-          <Route path='/*' element={restaurant?.usersRestaurant ? <Admin /> : <CreateRestaurantForm />} />
+          <Route path='/*' element={restaurant?.usersRestaurant ? <Admin /> : (auth.user ? <CreateRestaurantForm /> : (navigate('/account/login')))} />
         </Route>
       </Routes>
     </div>
--- Frontend/src/component/Home/CarouselItem.jsx
+++ Frontend/src/component/Home/CarouselItem.jsx
@@ -5,7 +5,7 @@ const CarouselItem = ({ image, title }) => {
 
         <div className='flex flex-col justify-center items-center'>
             <img src={image} alt="" className='w-[10rem] h-[10rem] lg:h-[14rem] lg:w-[14rem] rounded-full object-cover object-center' />
-            <span className='py-5 font-semibold text-xl text-gray-400'>{title}</span>
+            <span className='py-5 font-semibold  text-lg text-gray-400'>{title}</span>
         </div>
 
     )
