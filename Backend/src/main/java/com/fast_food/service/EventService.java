3bf1e41bb879861f2c19923f06843bc32ca40769
nuflait <nufla2017@gmail.com>
2024-06-12 10:17:09 +0530

    EventService added

---
+++ Backend/src/main/java/com/fast_food/service/EventService.java
@@ -0,0 +1,20 @@
+package com.fast_food.service;
+
+import com.fast_food.model.Event;
+
+import java.util.List;
+
+
+public interface EventService {
+
+    public Event createEvent(Event event, Long id) throws Exception;
+
+    public List<Event> getAllEvents() throws Exception;
+
+    public Event deleteEvent(Event event, Long id) throws Exception;
+
+    public Event updateEvent(Event event, Long id) throws Exception;
+
+    public Event getEvent(Event event, Long id) throws Exception;
+
+}
