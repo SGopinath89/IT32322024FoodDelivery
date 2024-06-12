d17dc95ac8a288f57272b9d7c8a973d8c38f1dd9
nuflait <nufla2017@gmail.com>
2024-06-12 10:18:50 +0530

    FoodService files added

---
+++ Backend/src/main/java/com/fast_food/service/FoodService.java
@@ -0,0 +1,30 @@
+package com.fast_food.service;
+
+import com.fast_food.model.Category;
+import com.fast_food.model.Food;
+import com.fast_food.model.Restaurant;
+import com.fast_food.request.CreateFoodRequest;
+
+import java.util.List;
+
+
+public interface FoodService {
+
+    public Food createFood(CreateFoodRequest req, Category category, Restaurant restaurant);
+    public void deleteFood(Long foodId) throws Exception;
+
+    public List<Food> getRestaurantFood(Long restaurantId,
+                                        boolean isVegetarian,
+                                        boolean isNonVeg,
+                                        boolean isSeasonal,
+                                        String foodCategory
+    );
+
+    public List<Food> getRestaurantAllFood(Long restaurantId);
+
+    public List<Food> searchFood(String keyword);
+
+    public Food findFoodById(Long foodId) throws Exception;
+
+    public Food updateAvailability(Long foodId) throws Exception ;
+}
